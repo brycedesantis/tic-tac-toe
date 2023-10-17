9716d955ba66cc63578144b479e96c9dc81456e2
Bryce DeSantis <potreader@hotmail.com>
2023-10-17 15:57:21 -0400

    Add win condition and message

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ const gameboard = (function() {
     children.forEach((square, index) => {
         square.addEventListener('click', () => {
             square.textContent = game.activePlayer.symbol
-            alert(game.activePlayer.name)
-            game.switchPlayer()
+            board[index] = game.activePlayer.symbol
+            // alert(index)
             square.style.pointerEvents = 'none'
+            game.checkWinner()
+            game.switchPlayer()
         })
     })
 
@@ -36,9 +38,41 @@ const game = (() => {
     function switchPlayer() {
         this.activePlayer === player1 ? this.activePlayer = player2 : this.activePlayer = player1
     }
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    function checkWinner(){
+        let gameOverScreen = document.querySelector('.game-over-screen')
+        let gameOverMessage = document.querySelector('#game-over-message')
+        let container = document.querySelector('.container')
+
+        winConditions.forEach((condition) => {
+            if(gameboard.board[condition[0]] === this.activePlayer.symbol && gameboard.board[condition[1]] === this.activePlayer.symbol && gameboard.board[condition[2]] === this.activePlayer.symbol) {
+                gameOverMessage.textContent = `${this.activePlayer.name} Wins!`
+                gameOverScreen.style.display = 'flex'
+                container.classList.add('blur')
+            }
+        })
+    }
+
+    function checkTie(){
+        
+    }
+
     return{
         activePlayer,
-        switchPlayer
+        switchPlayer,
+        checkWinner,
+        checkTie
     }
 })();
 
