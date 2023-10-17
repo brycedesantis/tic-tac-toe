a04085c407bf629cd4c0c4d4c716b0c98c54c5d1
Bryce DeSantis <potreader@hotmail.com>
2023-10-17 11:50:09 -0400

    Add player switch logic and symbol placement

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const gameboard = (function() {
     board.forEach((square, index) => {
         let squareElement = document.createElement('div')
         squareElement.className = 'square'
+        squareElement.setAttribute('id', `square-${index}`)
+        // squareElement.textContent = `${square}`
         htmlBoard.appendChild(squareElement)
+    })
+
+    let children = htmlBoard.childNodes
+
+    children.forEach((square, index) => {
+        square.addEventListener('click', () => {
+            square.textContent = game.activePlayer.symbol
+            alert(game.activePlayer.name)
+            game.switchPlayer()
+            square.style.pointerEvents = 'none'
+        })
     })
 
     return {
@@ -14,6 +27,21 @@ const gameboard = (function() {
     }
 })();
 
-function player() {
+const game = (() => {
+    let player1 = player('Player 1', 'x')
+    let player2 = player('Player 2', 'o')
+
+    let activePlayer = player1
     
+    function switchPlayer() {
+        this.activePlayer === player1 ? this.activePlayer = player2 : this.activePlayer = player1
+    }
+    return{
+        activePlayer,
+        switchPlayer
+    }
+})();
+
+function player(name, symbol) {
+    return {name, symbol}
 }
